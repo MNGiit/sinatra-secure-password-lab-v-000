59e693d62b09116489bc4925e8feead6f9c1a854
Mohammed Noori <kmanoorib@gmail.com>
2019-01-30 18:58:06 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_users.rb
+++ db/migrate/01_create_users.rb
@@ -5,6 +5,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :username
       # t.string :password # don't need to add this if adding password_digest column
       # password_digest is needed with has_secure_password inside user.rb
+      t.string :password_digest
     end
   end
   
