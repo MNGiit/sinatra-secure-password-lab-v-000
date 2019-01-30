52b99fd961714aa936f6c71e9e846c7f8db5a982
Mohammed Noori <kmanoorib@gmail.com>
2019-01-30 19:03:46 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -54,7 +54,7 @@ class ApplicationController < Sinatra::Base
     # if it isn't correct, redirect to failure
     # and use authenticate method for password checking
     @user = User.find_by(username: params[:username])
-    if @user.authenticate(params[:password]) && params[:password] != ""
+    if @user.authenticate(params[:password]) && @user
       session[:user_id] = @user.id
       redirect to "/account"
     else
